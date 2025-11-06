bb954dc454262ac7e0e8fedfe001ba725a6e85e7
Aitor <soy@aitor.me>
2025-11-06 17:49:01 +0500

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://mails-web.pages.dev",
+  site: "https://freedivemap.com",
   adapter: cloudflare(),
   prefetch: true,
   vite: {
